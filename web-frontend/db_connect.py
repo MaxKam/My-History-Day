993fca4afa89c41563b3e1654c6f34a1c543f915
Max <maxk893@gmail.com>
2017-12-31 20:37:18 -0800

    Added flask-sqlalchemy class

---
+++ web-frontend/db_connect.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    googleID = db.Column(db.String, nullable=False, unique=True)
+    firstName = db.Column(db.String(120), nullable=False)
+    lastName = db.Column(db.String(120), nullable=False)
+    googleCredentials = db.Column(db.Text)
+
+    def __repr__(self):
+        return 'User: %s \n' % self.fullName
+
+    def is_authenticated(self):
+        return True
+ 
+    def is_active(self):
+        return True
+ 
+    def is_anonymous(self):
+        return False
+ 
+    def get_id(self):
+        return self.id
