4b0a11dc845fc8b14efe5bfe8f90911848d008d1
Masya120 <maxk893@gmail.com>
2018-01-16 20:32:55 -0800

    Added OAuth2 process to app.
    Set up routes to take user through OAuth2 process to get authorization to access
    users calendar. App now requests additional scopes after user signed in per Google best practices.
    For now, storing OAuth2 token received from Google in users session cookie. This will be removed later.

--- history_day.py
+++ history_day.py
@@ -9,6 +9,7 @@ import os
 import json
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
+import googleapiclient.discovery
 
 #Instantiate ConfigParser and point to config file
 config = ConfigParser()
@@ -69,13 +70,60 @@ def login():
         registered_user = User.query.filter_by(googleID=userid).first()
 
       login_user(registered_user, remember=True)
-      return url_for('events')
+      return url_for('index')
 
 
 @app.route('/events')
 @login_required
 def events():
-  return render_template('events.html', MY_CLIENT_ID=MY_CLIENT_ID)
+  if 'credentials' not in session:
+    return redirect('authorize')
+  else:
+    return render_template('events.html', MY_CLIENT_ID=MY_CLIENT_ID)
+
+@app.route('/authorize')
+def authorize():
+  # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      CLIENT_SECRETS_FILE, scopes=SCOPES)
+
+  flow.redirect_uri = url_for('oauth2callback', _external=True)
+
+  authorization_url, state = flow.authorization_url(
+      # Enable offline access so that you can refresh an access token without
+      # re-prompting the user for permission. Recommended for web server apps.
+      access_type='offline',
+      # Enable incremental authorization. Recommended as a best practice.
+      include_granted_scopes='true')
+
+  # Store the state so the callback can verify the auth server response.
+  session['state'] = state
+
+  return redirect(authorization_url)
+
+
+@app.route('/oauth2callback')
+def oauth2callback():
+  # Specify the state when creating the flow in the callback so that it can
+  # verified in the authorization server response.
+  state = session['state']
+
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+  flow.redirect_uri = url_for('oauth2callback', _external=True)
+
+  # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+  authorization_response = request.url
+  flow.fetch_token(authorization_response=authorization_response)
+
+  # Store credentials in the session.
+  # ACTION ITEM: In a production app, you likely want to save these
+  #              credentials in a persistent database instead.
+  credentials = flow.credentials
+  session['credentials'] = credentials_to_dict(credentials)
+
+  return redirect(url_for('events'))
+
 
 @app.route("/logout")
 @login_required
