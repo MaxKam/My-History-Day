961c937151fa5b8a7e935b6aea2acc185948ef1f
Max K <maxk893@gmail.com>
2018-11-03 13:26:17 -0700

    Frontend service: Added two locations to check for rpc classes
    If app is running in Docker, rpc classes will be located in ./rpc_classes.
    If app is running locally, not in Docker, then rpc classes will be located
    in ../protos. Add both locations to syspath on app startup.

--- frontend_server/app.py
+++ frontend_server/app.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append("./rpc_classes")
+sys.path.append("../protos")
 
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, jsonify
